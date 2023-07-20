dbf6232339b3cc2ec96af3894ae3197def208190
Bach Dang <bachdang080@gmail.com>
2023-07-20 15:23:11 +0700

    cho dep

--- cypress/integration/examples/test.js
+++ cypress/integration/examples/test.js
@@ -23,13 +23,8 @@ describe('My first test suite', function() {
         Workspace.createWorkspace(workspaceName);
         cy.get('.cdk-overlay-backdrop').click();
         cy.wait(500);
-
-        //Đổi language sang tiếng Anh
-        cy.get('.wgc-basic-button__content > .wgc-avatar > .wgc-avatar__image').click();
-        cy.get('[icon="globe"] > .wgc-menu-item__content').click();
-        cy.xpath("//div[normalize-space()='English']").click();
-        cy.get('.cdk-overlay-backdrop').click();
-        cy.wait(500);
+        User.changeLanguage();
+        
         /*User.goToRoleAndPermission();
         //invite user
         for(let i = 2; i <= 20; i++) {
@@ -45,9 +40,9 @@ describe('My first test suite', function() {
             cy.get('[wgcdialogheader=""] > .wgc-primary > .wgc-button-wrapper').click();
             cy.wait(500);
         }
-        cy.wait(500);*/
-        // User.goToRoleAndPermission();
-        // User.createRole();
+        cy.wait(500);
+        User.goToRoleAndPermission();
+        User.createRole();*/
         Collection.createCollection();
         
         cy.wait(500);
@@ -82,139 +77,13 @@ describe('My first test suite', function() {
         // CreateField.lookupField();
         // CreateField.lastModifiedByField();
         // CreateField.lastModifiedTimeField();
-        for (let i = 1; i <= 100; i++) {
+        for (let i = 1; i <= 20; i++) {
             cy.get('.inserting-row__left > .wgc-basic-button > .wgc-basic-button-wrapper > .wgc-basic-button__content').scrollIntoView().click();
             const rowNumber = "row " + i;
             cy.get('.ng-invalid').type(rowNumber);
             cy.get('.inserting-row__right > .wgc-primary > .wgc-button-wrapper').click();
             cy.wait(500);
         }
-        
-        
-        //qua mailinator
-            // cy.visit('https://www.mailinator.com/v4/public/inboxes.jsp?trialshow=true')
-            // cy.wait(1000)
-            // cy.get('#requestTrial').type('{esc}')
-            // const email_invite = "bachdang1{enter}"
-            // cy.get('#inbox_field').type(email_invite)
-            // cy.get('[style="float:left;margin-top: 10px;overflow:hidden;"] > .fw-700').click()
-            // cy.get("tr[id='row_bachdang1-1687855083-230049656'] td:nth-child(3)").click()
-            // cy.get('#pills-links-tab').click()
-            // cy.get('tbody > :nth-child(1) > :nth-child(2) > a').click()
-
-            // cy.get('.wgc-form-field__input-container > .wgc-form-field__input > .ng-invalid').type("Dang 1");
-            
-
-        
-        
-
-        /*
-        // Tạo list view
-        cy.get("button[class='wgc-icon-button board-function__btn-action wgc-icon-button--small ng-star-inserted'] div[class='wgc-icon-button-wrapper']").click();
-        cy.wait(500)
-        cy.get('.mt-5 > .wgc-radio__label').click();
-        cy.wait(500)
-        //button new
-        cy.get('.wgc-primary > .wgc-button-wrapper').click();
-        cy.wait(500)
-        // enter view name
-        cy.get('.wgc-form-field__input > .ng-invalid').type("Check list");
-        cy.wait(500)
-        cy.get('.wgc-primary > .wgc-button-wrapper').click();
-        //cy.get('.wgc-form-field.mb-30 > .wgc-form-field__input-container > .wgc-form-field__suffix').click();
-        cy.wait(500)
-        // tạo form view
-        cy.get("button[class='wgc-icon-button board-function__btn-action wgc-icon-button--small ng-star-inserted'] div[class='wgc-icon-button-wrapper']").click();
-        cy.wait(500)
-        cy.get('.mt-10 > .wgc-radio__circle').click();
-        cy.wait(500)
-        //button new
-        cy.get('.wgc-primary > .wgc-button-wrapper').click();
-        cy.wait(500)
-        // type name
-        cy.get('.wgc-form-field__input > .ng-invalid').type("Check form");
-        cy.wait(500)
-        cy.get('.wgc-primary > .wgc-button-wrapper').click(); 
-
-        // Tạo list view
-        cy.get('.color-label > :nth-child(1) > :nth-child(1) > :nth-child(1) > .wgc-basic-button > .wgc-basic-button-wrapper > .wgc-basic-button__icon > .wgc-icon > .icon').click();
-        cy.wait(500)
-        cy.get('.mv-15 > .wgc-basic-button-wrapper > .wgc-basic-button__content').click();
-        cy.wait(500)
-        cy.get('.mt-5 > .wgc-radio__label').click();
-        cy.wait(500)
-        //button new
-        cy.get('[wgcdialogheader=""] > .wgc-primary > .wgc-button-wrapper').click();
-        cy.wait(500)
-        // enter view name
-        cy.get('.wgc-form-field__input > .ng-invalid').type("Check list");
-        cy.wait(500)
-        cy.get('[wgcdialogheader=""] > .wgc-primary > .wgc-button-wrapper').click();
-        //cy.get('.wgc-form-field.mb-30 > .wgc-form-field__input-container > .wgc-form-field__suffix').click();
-        cy.wait(500)
-
-        // tạo form view
-        cy.get('.color-label > :nth-child(1) > :nth-child(1) > :nth-child(1) > .wgc-basic-button > .wgc-basic-button-wrapper > .wgc-basic-button__icon > .wgc-icon > .icon').click();
-        cy.get('.mv-15 > .wgc-basic-button-wrapper > .wgc-basic-button__content').click();
-        //Chọn formview
-        cy.get('.mt-10 > .wgc-radio__circle').click();
-        cy.wait(500)
-        //button new
-        cy.get('[wgcdialogheader=""] > .wgc-primary > .wgc-button-wrapper').click();
-        cy.wait(500)
-        // type name
-        cy.get('.wgc-form-field__input > .ng-invalid').type("Check form");
-        cy.wait(500)
-        cy.get('[wgcdialogheader=""] > .wgc-primary > .wgc-button-wrapper').click();
-
-        cy.get('.color-label > :nth-child(1) > :nth-child(1) > :nth-child(1) > .wgc-basic-button > .wgc-basic-button-wrapper > .wgc-basic-button__icon > .wgc-icon > .icon').click();
-        cy.get(':nth-child(1) > .tab-arrangement > .view-function > .wgc-menu-item').trigger('mouseover')
-        cy.get(':nth-child(1) > .tab-arrangement > .view-function > .wgc-menu-item > .wgc-menu-item__suffix > .mr-5 > .wgc-basic-button-wrapper > .wgc-basic-button__icon > .wgc-icon > .icon').click();
-        //cy.get(':nth-child(2) > .tab-arrangement > .view-function > .wgc-menu-item > .wgc-menu-item__suffix > .mr-5 > .wgc-basic-button-wrapper > .wgc-basic-button__icon > .wgc-icon > .icon')
-        
-        cy.get('.color-label > :nth-child(1) > :nth-child(1) > :nth-child(1) > .wgc-basic-button > .wgc-basic-button-wrapper > .wgc-basic-button__icon > .wgc-icon > .icon').click();
-        cy.get(':nth-child(2) > .tab-arrangement > .view-function > .wgc-menu-item > .wgc-menu-item__suffix > .mr-5 > .wgc-basic-button-wrapper > .wgc-basic-button__icon > .wgc-icon')
-            .trigger('mouseover')
-            .click();
-        
-        cy.get('.color-label > :nth-child(1) > :nth-child(1) > :nth-child(1) > .wgc-basic-button > .wgc-basic-button-wrapper > .wgc-basic-button__icon > .wgc-icon > .icon').click();
-        cy.get(':nth-child(3) > .tab-arrangement > .view-function > .wgc-menu-item > .wgc-menu-item__suffix > .mr-5 > .wgc-basic-button-wrapper > .wgc-basic-button__icon > .wgc-icon')
-            .trigger('mouseover')
-            .click();
-
-            cy.xpath("//p[normalize-space()='test 4 premium']").click();
-            cy.wait(1000);
-            Collection.createCollection();
-            cy.wait(2000);
-            Board.createBoard();
-            cy.wait(2000);
-            CreateField.dateField();
-            cy.wait(1000);
-            //Tạo field Dropdown cho board
-            const dropName = "Field Dropdown";
-            const dropDes = "Des cùa field Dropdown";
-            CreateField.dropdownField(dropName, dropDes);
-            
-            
-        //Dang ký ngoài 
-        for (let i = 1; i <= 100; i++) {
-            cy.visit('https://line1.8aday.com/');
-            cy.get(':nth-child(2) > a').click();
-            const email_invite = "bachdang" + i + "@mailinator.com";
-            cy.get('.wgc-form-field__input > .ng-invalid').type(email_invite);
-            cy.get('.mb-25.ng-dirty > .wgc-button > .wgc-button-wrapper').click();
-            cy.wait(500)
-        }
-
-            */
-        
-        // for (let i = 1; i <= 100; i++) {
-        //     cy.get('.inserting-row__left > .wgc-basic-button > .wgc-basic-button-wrapper > .wgc-basic-button__content').scrollIntoView().click();
-        //     const rowNumber = "row " + i;
-        //     cy.get('.ng-invalid').type(rowNumber);
-        //     cy.get('.inserting-row__right > .wgc-primary > .wgc-button-wrapper').click();
-        //     cy.wait(500);
-        // }
 
     })
 
--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,13 +1,15 @@
 Cypress.Commands.add('login', () => {
     // Thực hiện các bước đăng nhập:
     cy.wait(500);
-    //cy.visit("https://line1.8aday.com/");
+    cy.visit("https://line1.8aday.com/");
     //cy.visit('https://workblink.com/');
-    cy.visit('https://workgot.vn/');
+    //cy.visit('https://workgot.vn/');
     cy.wait(500);
-    cy.xpath("//input[@placeholder='Nhập email của bạn']").type("dang.do@cubable.com")
+    cy.xpath("//input[@placeholder='Nhập email của bạn']").type("dang.do@cubable.com");
+    cy.xpath("//input[@placeholder='Type your email']").type("dang.do@cubable.com");
     //cy.xpath('//input[@placeholder="Type a password"]').type("1");
-    cy.xpath("//input[@placeholder='Nhập mật khẩu']").type("Bachdang88");
+    cy.xpath("//input[@placeholder='Nhập mật khẩu']").type("1");
+    cy.xpath("//input[@placeholder='Type a password']").type("1");
     cy.get("button[type='submit'] div[class='wgc-button-wrapper']").click();
     cy.wait(500);
     //cy.xpath('//body[1]/app[1]/section[1]/sign-in[1]/div[1]/div[1]/div[1]/div[1]/div[2]/wgc-scroll-bar[1]/table[1]/tbody[1]/tr[2]/td[1]/div[1]/div[1]').click()
